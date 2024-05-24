d6c2b5f44989f78c0ca9b9ba8c0b6dd2596035cd
Leeves <leeves.chou@gmail.com>
2024-05-24 10:06:34 +0930

    Quiz project useContext

--- 05_React-Quiz/src/QuizContext.jsx
+++ 05_React-Quiz/src/QuizContext.jsx
@@ -1,20 +1,77 @@
 import { createContext, useContext, useEffect } from "react";
 
+// initiate reducer value
+const initialValue = {
+  questions: [],
+  // dataReceived, loading, error
+  status: "loading",
+  quizIndex: 0,
+  answer: null,
+  points: 0,
+};
+// reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived": // success for fetching data
+      return { ...state, status: "dataReceived", questions: action.payload };
+    case "dataFailed": // failed for fetching data
+      return { ...state, status: "error" };
+    case "quizStart":
+      return {
+        ...state,
+        status: "quizStart",
+        answer: null,
+        quizIndex: 0,
+        points: 0,
+      };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === state.questions[state.quizIndex].correctOption
+            ? state.points + state.questions[state.quizIndex].points
+            : state.points,
+      };
+    case "nextQuiz":
+      if (state.quizIndex === state.questions.length - 1)
+        return { ...state, status: "finish" };
+      return { ...state, quizIndex: state.quizIndex + 1, answer: null };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 // initiate context
 const QuizContext = createContext();
 
 function QuizProvider({ children }) {
-  useEffect(
-    function () {
-      fetch("http://localhost:8000/questions")
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-    },
-    []
+  const [{ questions, status, quizIndex, answer, points }, dispatch] =
+    useReducer(reducer, initialValue);
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (acc, curr) => acc + curr.points,
+    0
   );
 
-  return <QuizContext.Provider value={{
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
+  return <QuizContext.Provider value={{
+    status,
+    answer,
+    quizIndex,
+    points,
+    numQuestions,
+    maxPossiblePoints,
+    question: questions[quizIndex],
+    onStart: () => dispatch({ type: "quizStart"}),
+    onAnswer: (answerID) => dispatch({ type: "newAnswer", payload: answerID }),
+    onNext: () => dispatch({ type: "nextQuiz" }),
+    onRestart: () => dispatch({ type: "quizStart" }),
   }}>{children}</QuizContext.Provider>;
 }
 
